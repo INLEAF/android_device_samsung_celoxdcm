b05cf076cc58053d3fa29b9e2de2863501e33599
Andros Rosa <andros2011@gmail.com>
2014-01-24 20:12:32 -0400

    Fresh carbonized tree
    - added bootanimation dimensions
    
    Change-Id: I71d81c035eab3992f1c78d5a312cf2911b7f27a4

--- carbon.mk
+++ carbon.mk
@@ -1,16 +1,13 @@
 $(call inherit-product, device/samsung/hercules/full_hercules.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.4/KRT16M/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.4 KRT16M UVMC6 release-keys"
 
-PRODUCT_NAME := cm_hercules
+PRODUCT_NAME := carbon_hercules
 PRODUCT_DEVICE := hercules
-
