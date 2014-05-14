ef15fc30e2729aeb65fa99064061f7ab03cbbffb
Rabih Salamey <somcom3@gmail.com>
2014-05-14 07:23:07 -0400

    Fix CarbonAPP RomList
    Remove override
    
    Change-Id: Ife6bee15bf29b71d6b4e0356d9dafbb911b3287a

--- carbon.mk
+++ carbon.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/samsung/hercules/full_hercules.mk)
 # Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.4/KRT16M/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.4 KRT16M UVMC6 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.4/KRT16M/T989UVMC6:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.4 KRT16M UVMC6 release-keys"
 
 PRODUCT_NAME := carbon_hercules
 PRODUCT_DEVICE := hercules
