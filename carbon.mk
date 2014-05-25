dmascord/android_device_samsung_e110s

Fresh carbonized tree
- added bootanimation dimensions

Change-Id: I6d6b948a2718185904f151e34ef30fc856797917

carbon.mk
@@ -1,16 +1,14 @@
 $(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.1.2/JZO54K/I727UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.1.2 JZO54K I727UCMC1 release-keys"
 
-PRODUCT_NAME := cm_skyrocket
+PRODUCT_NAME := carbon_skyrocket
 PRODUCT_DEVICE := skyrocket
 

INLEAF/android_device_samsung_celoxdcm

Conflicts:
	carbon.mk
	cm.dependencies
	vendorsetup.sh

carbon.mk
@@ -1,16 +1,14 @@
 $(call inherit-product, device/samsung/celoxdcm/full_celoxdcm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Carbon stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SC-03D TARGET_DEVICE=SC-03D BUILD_FINGERPRINT="samsung/SC-03D/SC-03D:4.4/KRT16M/OMLPH:user/release-keys" PRIVATE_BUILD_DESC="SC-03D-user 4.4 KRT16M OMLPH release-keys"
 
-PRODUCT_NAME := cm_celoxdcm
+PRODUCT_NAME := carbon_celoxdcm
 PRODUCT_DEVICE := celoxdcm
 